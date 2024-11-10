773bf322aa568f5eed86f72700d895c123a2b8bd
Fernando Jorge <fernando.jorge@orla.tech>
2024-11-10 10:33:42 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Fernando Jorge on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
