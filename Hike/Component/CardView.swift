628d08e3361b8c6d6e6d5a68864fdef23c97ebe8
Fernando Jorge <fernando.jorge@orla.tech>
2024-11-10 21:29:25 -0300

    feat: custom button view

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,19 +12,48 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [.customIndigoMedium, .customSalmonLight],
-                            startPoint: .top,
-                            endPoint: .bottom
-                    )).frame(width: 256, height: 256)
+            VStack {
+                // MARK - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking").fontWeight(.black).font(.system(size: 52)).foregroundStyle(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            // ACTION - SHOW A SHEET
+                            print("The button was pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.").multilineTextAlignment(.leading).italic().foregroundColor(.customGrayMedium)
+                }.padding(.horizontal, 30)
+                
+                // MARK - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [.customIndigoMedium, .customSalmonLight],
+                                startPoint: .top,
+                                endPoint: .bottom
+                        )).frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK - FOOTER
+                
             }
+            
+            
         }
         .frame(width: 320, height: 570)
     }
